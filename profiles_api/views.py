156a062999f925a1a7bce588bf74c3bffe627995
Shi <stoneshishang@gmail.com>
2021-03-19 13:42:34 -0400

    test change of email address

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -93,3 +93,5 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+
+    # Test for github name change.
